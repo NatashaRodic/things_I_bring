3105b9ba0a6a80359ac39097bca58107899f8bbe
Natasha Rodic <natashawolt@gmail.com>
2023-09-20 22:37:56 -0400

    Enhance list items with images and style 'Bring Items' list

--- script.js
+++ script.js
@@ -48,43 +48,50 @@ const items = [
         itemName: 'ID or passport',
         weather: 'All',
         daysInUse: 365,
-        essentials: true
+        essentials: true,
+        itemImage:'images/passport.png'
     },
     {
         itemName: 'Boarding passes or tickets',
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/boarding-passes.png'
     },
     {
         itemName: 'Hotel reservation confirmation',
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/hotel-reservation-confirmation.png'
     },
     {
         itemName: 'Credit/debit cards',
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/debit-cards.png'
     },
     {
         itemName: 'Phone and charger',
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/phone.png'
     },
     {
         itemName: 'Power bank',
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/power-bank.png'
     },
     {
         itemName: 'Laptop/tablet and charger',
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/tablet.png'
     },
     
 
@@ -93,12 +100,14 @@ const items = [
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage:'images/keys.png'
     },
     {
         itemName: 'Sunglasses',
         weather: 'Hot',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/sunglasses.png'
     },
     
     {
@@ -106,6 +115,7 @@ const items = [
         weather: 'All',
         daysInUse: 365,
         essentials: true,
+        itemImage: 'images/hand-sanitizer.png'
     },
    
     {
@@ -113,63 +123,72 @@ const items = [
         weather: 'All',
         daysInUse: 2,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/tops.png'
     },
     {
         itemName: 'Pants/Shorts',
         weather: 'All',
         daysInUse: 3,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/pants.png'
     },
     {
         itemName: 'Sleepwear',
         weather: 'All',
         daysInUse: 365,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/sleepwear.png'
     },
     {
         itemName: 'Socks',
         weather: 'All',
         daysInUse: 1,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/socks.png'
     },
     {
         itemName: 'Jacket',
         weather: 'Cold',
         daysInUse: 365,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/jacket.png'
     },
     {
         itemName: 'Comfortable Shoes',
         weather: 'All',
         daysInUse: 365,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/shoes.png'
     },
     {
         itemName: 'Swimwear',
         weather: 'Hot',
         daysInUse: 365,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/swimwear.png'
     },
     {
         itemName: 'Hat',
         weather: 'Hot',
         daysInUse: 365,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/hat.png'
     },
     {
         itemName: 'Umbrella',
         weather: 'Cold',
         daysInUse: 365,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/umbrella.png'
     },
     
     {
@@ -177,14 +196,16 @@ const items = [
         weather: 'All',
         daysInUse: 1,
         essentials: false,
-        style: 'unisex'
+        style: 'unisex',
+        itemImage: 'images/underwear.png'
     },
     {
         itemName: 'Dress',
         weather: 'Hot',
         daysInUse: 3,
         essentials: false,
-        style: 'women'
+        style: 'women',
+        itemImage: 'images/dress.png'
     }
 ]
 
@@ -213,7 +234,8 @@ const bringEssentials = function (essentialsBring, items, days) {
             essentialsBring.push(
                 {
                     itemName: item.itemName,
-                    itemQuantity: howManyByDays(item.daysInUse, days)
+                    itemQuantity: howManyByDays(item.daysInUse, days),
+                    itemImage: item.itemImage 
                 }
 
             )
@@ -234,7 +256,8 @@ const bringWeatherItems = function (itemsBring, items, vrijeme, days, style) {
             itemsBring.push(
                 {
                     itemName: item.itemName,
-                    itemQuantity: howManyByDays(item.daysInUse, days)
+                    itemQuantity: howManyByDays(item.daysInUse, days),
+                    itemImage: item.itemImage 
                 }
             )
             
@@ -270,10 +293,10 @@ function displayDate (event) {
     let cardOutput ="";
 
     bringEssentials(essentialsBring,items,days).forEach(item=>{
-        cardOutput+= `  <div class="item-card">
-        <div class="item">
-            <img src="https://picsum.photos/600/400" alt="">
-            <p>${item.itemName}</p>
+        cardOutput+= `  <div class="item-card" >
+        <div class="item" >
+           <div class="imgAndText">  <img src="${item.itemImage}" alt="">
+           <p>  ${item.itemName} ❗️</p></div>
             <div class="checkbox-container">
                 <input type="checkbox" id="${item.itemName}">
                 <label for="${item.itemName}"></label>
@@ -286,8 +309,8 @@ function displayDate (event) {
     bringWeatherItems(itemsBring, items, printLocationWeather(location, locationWeather), days, styleButton()).forEach(item => {
         cardOutput+= `  <div class="item-card">
         <div class="item">
-            <img src="https://picsum.photos/600/400" alt="">
-            <p>${item.itemName} x ${item.itemQuantity}</p>
+           <div class="imgAndText"> <img src="${item.itemImage}" alt="">
+           <p>${item.itemName} x <strong>${item.itemQuantity} </strong></p></div>
             <div class="checkbox-container">
                 <input type="checkbox" id="${item.itemName}">
                 <label for="${item.itemName}"></label>
@@ -296,7 +319,6 @@ function displayDate (event) {
     </div>`
 
     })
-
     document.getElementById('bringItems').innerHTML = cardOutput;
     
     document.getElementById('bringItems').style.display="flex";
